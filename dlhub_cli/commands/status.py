d6c130f0ecb5c22f2ad9f9c9934226b2bbc2245f
Ryan <rchard@anl.gov>
2018-10-25 14:08:42 -0500

    Status updates.

---
+++ dlhub_cli/commands/status.py
@@ -0,0 +1,33 @@
+import os
+import json
+import uuid
+import boto3
+import click
+import requests
+import pickle as pkl
+
+import dlhub_toolbox
+import dlhub_client
+
+from dlhub_cli.config import get_dlhub_directory, get_publish_url, get_dlhub_client
+from dlhub_cli.printing import format_output
+from dlhub_cli.parsing import dlhub_cmd
+
+
+@dlhub_cmd('status', help='Check the status of a DLHub task.')
+@click.option('--task',
+              show_default=True, required=True,
+              help='The UUID of a task.')
+def status_cmd(task):
+    """
+    Use DLHub to query the status of a task.
+
+    :param task: The uuid of a task
+    :return:
+    """
+    format_output("Checking status {}".format(task))
+
+    client = get_dlhub_client()
+    res = client.get_task_status(task)
+
+    format_output("{0}: {1}".format(task, res))
