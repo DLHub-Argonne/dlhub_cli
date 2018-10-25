90a2c0bede6b3cd3189a155f6720765347bdc792
Ryan <rchard@anl.gov>
2018-10-25 14:50:35 -0500

    Adding initial run functionality.

--- dlhub_cli/commands/run.py
+++ dlhub_cli/commands/run.py
@@ -2,20 +2,42 @@ import json
 import click
 
 from dlhub_cli.printing import format_output
-# from dlhub_cli.config import get_dlhub_client
-from dlhub_cli.parsing import dlhub_cmd, index_argument
+from dlhub_cli.config import get_dlhub_client
+from dlhub_cli.parsing import dlhub_cmd
 
 
 @dlhub_cmd('run', help='Invoke a servable')
 @click.option('--servable',
-              default=None, show_default=True,
+              default=None, show_default=True, required=False,
               help='The servable to invoke.')
-def run_cmd(servable):
+@click.option('--servable-uuid',
+              default=None, show_default=True, required=False,
+              help='The uuid of the servable to invoke.')
+@click.option('--input',
+              default=None, show_default=True, required=False,
+              help='Input to pass to the servable.')
+@click.option('--test',
+              is_flag=True,
+              help='Flag whether or not to invoke the servable\'s test function.')
+def run_cmd(servable, servable_uuid, input, test):
     """
-    Invoke the servable
+    Invoke a servable.
 
     :param servable: The servable to invoke
+    :param servable_uuid: The uuid of the servable to invoke
+    :param input: Input to pass into the servable
+    :param test: Whether or not to use the test function
     :return:
     """
     format_output("Running {}".format(servable))
-    pass
+
+    if test:
+        format_output("Invoking test function")
+
+    client = get_dlhub_client()
+
+    data = json.loads(input)
+
+    res = client.run(servable_uuid, data)
+
+    format_output(res)
